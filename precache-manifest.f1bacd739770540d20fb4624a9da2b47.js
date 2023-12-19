52d6a942179972bc9aad7c899291b4df34c5f77d
Roger <00857002@email.ntou.edu.tw>
2023-12-19 12:47:25 +0800

    Updates

---
+++ precache-manifest.f1bacd739770540d20fb4624a9da2b47.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e930fed2609b1f35d9de0855c1b3fec",
+    "url": "/react-api-demo/index.html"
+  },
+  {
+    "revision": "4cc986fbd745b728c25d",
+    "url": "/react-api-demo/static/css/main.427275ce.chunk.css"
+  },
+  {
+    "revision": "861f05b2efe73045405b",
+    "url": "/react-api-demo/static/js/2.ff1402c0.chunk.js"
+  },
+  {
+    "revision": "e26de4f0e0d98f1626b38316e0457409",
+    "url": "/react-api-demo/static/js/2.ff1402c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbce2b01c4f7f46fdf7a",
+    "url": "/react-api-demo/static/js/3.f41744e1.chunk.js"
+  },
+  {
+    "revision": "4cc986fbd745b728c25d",
+    "url": "/react-api-demo/static/js/main.6f0c5078.chunk.js"
+  },
+  {
+    "revision": "f61f8fc88bd87bf5e82f",
+    "url": "/react-api-demo/static/js/runtime-main.4b6fdebc.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-api-demo/static/media/logo.06e73328.svg"
+  }
+]);
